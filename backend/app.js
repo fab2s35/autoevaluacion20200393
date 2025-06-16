b060f7e438ad5eec65e824065512b58bd4032a31
fab2s35 <sosorto72@gmail.com>
2025-06-16 15:27:47 -0600

    documentacion agregada

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,23 @@
 import express from "express";
 import tasksRoutes from "./src/routes/tasks.js";
-/*
+
 import swaggerUi from "swagger-ui-express";
 import fs from "fs";
 import path from "path";
-*/
+
 
 const app = express();
 
 app.use(express.json());
-/*
+
 const swaggerDocument = JSON.parse(
   fs.readFileSync(
-    path.resolve("./documentacion.json"),
+    path.resolve("./documentation.json"),
     "utf-8"
   )
 )
-  */
-/*app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));*/
+  
+app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/tasks", tasksRoutes);
 
 export default app;
